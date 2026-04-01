a2109d5f0b224575e11ca55f6352eb5b7db7eb32
Pavloxes <pavloxes@gmail.com>
2026-04-01 16:18:02 +0500

    Часть 09, создание класса панели

---
+++ Assembler_Project/Test_Asm/Test_Asm/Main/Main.cpp
@@ -0,0 +1,76 @@
+﻿#include "Main.h"
+
+//----------------------------------------------------------------------------------------------------------------------
+int main(void)
+{
+    HANDLE std_handle, screen_buffer_handle;
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info{};
+    CHAR_INFO* screen_buffer;
+    COORD screen_buffer_pos{};
+    int screen_buffer_size;
+
+    // Get a handle to the STDOUT screen buffer to copy from and
+    // create a new screen buffer to copy to.
+
+    std_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    screen_buffer_handle = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, CONSOLE_TEXTMODE_BUFFER, 0);
+    if (std_handle == INVALID_HANDLE_VALUE || screen_buffer_handle == INVALID_HANDLE_VALUE)
+    {
+        printf("CreateConsoleScreenBuffer failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    // Make the new screen buffer the active screen buffer.
+
+    if (!SetConsoleActiveScreenBuffer(screen_buffer_handle))
+    {
+        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+
+    if (!GetConsoleScreenBufferInfo(screen_buffer_handle, &screen_buffer_info))
+    {
+        printf("GetConsoleScreenBufferInfo failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    screen_buffer_size = (int)screen_buffer_info.dwSize.X * (int)screen_buffer_info.dwSize.Y;
+
+    screen_buffer = new CHAR_INFO[screen_buffer_size];
+    memset(screen_buffer, 0, screen_buffer_size * sizeof(CHAR_INFO));
+
+    // Copy from the temporary buffer to the new screen buffer.
+
+    //screen_buffer[0].Char.UnicodeChar = L'W';
+    //screen_buffer[0].Attributes = 0x50;
+
+
+    unsigned short half_width = screen_buffer_info.dwSize.X / 2;
+
+    APanel left_panel(0, 0, half_width, screen_buffer_info.dwSize.Y - 2, screen_buffer, screen_buffer_info.dwSize.X);
+    //APanel right_panel(half_width, 0, half_width, screen_buffer_info.dwSize.Y - 2);
+
+    left_panel.Draw();
+    //right_panel.Draw();
+
+
+    if (!WriteConsoleOutput(screen_buffer_handle, screen_buffer, screen_buffer_info.dwSize, screen_buffer_pos, &screen_buffer_info.srWindow))
+    {
+        printf("WriteConsoleOutput failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    Sleep(150 * 1000);
+
+    // Restore the original active screen buffer.
+
+    if (!SetConsoleActiveScreenBuffer(std_handle))
+    {
+        printf("SetConsoleActiveScreenBuffer failed - (%d)\n", GetLastError());
+        return 1;
+    }
+
+    return 0;
+}
+//----------------------------------------------------------------------------------------------------------------------
---
+++ Assembler_Project/Test_Asm/Test_Asm/Panel/Panel.cpp
@@ -0,0 +1,24 @@
+#include "Panel.h"
+
+//----------------------------------------------------------------------------------------------------------------------
+APanel::APanel(unsigned short x_pos, unsigned short y_pos, unsigned short width, unsigned short height, CHAR_INFO* screen_buffer, unsigned short screen_width)
+    : X_Pos(x_pos), Y_Pos(y_pos), Width(width), Height(height), Screen_Buffer(screen_buffer), Screen_Width(screen_width)
+{
+}
+//----------------------------------------------------------------------------------------------------------------------
+void APanel::Draw()
+{
+
+    CHAR_INFO symbol{};
+
+    symbol.Char.UnicodeChar = L'X';
+    symbol.Attributes = 0x1b;
+
+    SPos pos(50, 8, Screen_Width, 10);
+
+    //Draw_Line_Horizontal(Screen_Buffer, pos, symbol);
+    Draw_Line_Vertical(Screen_Buffer, pos, symbol);
+
+    //Show_Colors(Screen_Buffer, pos, symbol);
+}
+//----------------------------------------------------------------------------------------------------------------------
